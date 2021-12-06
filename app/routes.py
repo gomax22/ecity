436a55bdc9ed5c6bd28b316ecc83ad6ed54f67a4
Maax <massimiliano.giordanoorsini001@studenti.uniparthenope.it>
2021-12-06 13:09:46 +0100

    New file

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    return
+    return render_template('index.html')
